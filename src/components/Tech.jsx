a69e971370d495917f6135ee3f1374045a8c6190
PRERIT0007 <preritg050@gmail.com>
2025-10-05 18:57:02 +0530

    Refactored Ball, Computers, and Tech components to enhance responsiveness; replace image rendering in Tech with BallCanvas and implement mobile detection in Computers.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-// import { BallCanvas } from "./canvas";
+import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
@@ -9,7 +9,7 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <img src={technology.icon} alt={technology.name} />
+          <BallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
